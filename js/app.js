1e74ef4f2156a8bff34324d51f13d5d84bb60bab
Gus Viola <gus.viola@pobox.com>
2018-06-12 10:15:49 -0300

    Advance on Rendering

--- js/app.js
+++ js/app.js
@@ -1,25 +1,29 @@
-Player// Enemies our Player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+// Enemies our Player must avoid
+// canvas.width = 505;
+// canvas.height = 606;
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = "images/enemy-bug.png";
+const Enemy = function() {
+  // Variables applied to each of our instances go here,
+  // we've provided one for you to get started
+  this.x = 500;
+  this.y = 200;
+  // The image/sprite for our enemies, this uses
+  // a helper we've provided to easily load images
+  this.sprite = "images/enemy-bug.png";
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    this.x *= dt;
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
+  this.x *= dt;
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own Player class
@@ -29,23 +33,25 @@ Enemy.prototype.render = function() {
 
 //let sprite; //attribute of Player class
 
-var Player = function() {
-    this.sprite = "images/char-cat-girl.png";
-  };
+const Player = function() {
+  this.x = 200;
+  this.y = 400;
+  this.sprite = "images/char-cat-girl.png";
+};
 
 
 Player.prototype.update = function() {
   // console.log(this);
-    return null;
-  };
+  return null;
+};
 
 Player.prototype.render = function() { //draws Player on screen
-    console.log(this.sprite);
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-  };
+  console.log(this.sprite);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
-Player.prototype.handleInput = function() {//handles the key inputs for Player movement
-    return null;
+Player.prototype.handleInput = function() { //handles the key inputs for Player movement
+  return null;
 };
 
 // Now instantiate your objects.
@@ -60,12 +66,12 @@ let player = new Player;
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener("keyup", function(e) {
-    var allowedKeys = {
-        37: "left",
-        38: "up",
-        39: "right",
-        40: "down"
-    };
+  var allowedKeys = {
+    37: "left",
+    38: "up",
+    39: "right",
+    40: "down"
+  };
 
-    Player.handleInput(allowedKeys[e.keyCode]);
+  Player.handleInput(allowedKeys[e.keyCode]);
 });
